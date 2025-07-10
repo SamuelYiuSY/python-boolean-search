2cd2d1fe0a1843224f7ddd4d42a536e0a7f747dd
Samuel <48211508+SamuelYiuSY@users.noreply.github.com>
2025-07-10 16:55:35 +0100

    Rename corpus to string_to_search

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def boolean_search(corpus, AND_list = None, OR_list = None, NOT_list = None):
+def boolean_search(string_to_search, AND_list = None, OR_list = None, NOT_list = None):
     if AND_list:
         for eachWord in AND_list:
-            if eachWord not in corpus:
+            if eachWord not in string_to_search:
                 # print("AND statement not satisified")
                 return False
     
     if OR_list:
         have_1_OR_word_flag = False
         for eachWord in OR_list:
-            if eachWord in corpus:
+            if eachWord in string_to_search:
                 have_1_OR_word_flag = True
                 break
         if not have_1_OR_word_flag:
@@ -17,7 +17,7 @@ def boolean_search(corpus, AND_list = None, OR_list = None, NOT_list = None):
     
     if NOT_list:
         for eachWord in NOT_list:
-            if eachWord in corpus:
+            if eachWord in string_to_search:
                 # print("NOT statement not satisified")
                 return False
     
