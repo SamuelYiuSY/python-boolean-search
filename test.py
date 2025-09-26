e40c5c47e4a85d925d0ac739488afc7b39c5cb74
Samuel <48211508+SamuelYiuSY@users.noreply.github.com>
2025-09-26 14:39:44 +0100

    add long test cases

--- test.py
+++ test.py
@@ -1,41 +1,47 @@
 import main
+import time
 
 boolean_search = main.boolean_search
 
+one_hundred_animals_string = "Lion Tiger Bear Wolf dog rabbit Fox Squirrel cat Mouse Horse Cow Sheep Duck Goose Chicken Pig Chicken Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Bird Monkey Elephant Zebra Camel Gorilla Panda Koala Flamingo Hippo Shark Camelopardus Hamster Penguin Koala Bear Lion Leopard Cheetah Tiger Gorilla Gorilla Giraffe Elephant Rhino Orangutan Monkey Lion Tiger Eagle Wolf Bear Fox Deer Sheep Horse Cow Pig Chicken Duck Goose Rabbit Sheep Cow Horse Dog Wolf Lion Tiger Bear Fox Rabbit Cat Wolf Bear Fox Squirrel Dog Mouse Horse Cow Sheep Duck Goose Chicken Pig Lizard Snake Frog Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle"
 
-def test_basic_and_cases():
-    assert boolean_search("cat dog rabbit", ["cat", "rabbit"]) == True
-    assert boolean_search("dog mouse", ["cat", "rabbit"]) == False
+def test_basic_and_cases(search_whole_words_only = False):
+    assert boolean_search("cat dog rabbit", AND_list=["cat", "rabbit"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search(one_hundred_animals_string, AND_list=["cat", "rabbit"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search("dog mouse", AND_list=["cat", "rabbit"], search_whole_words_only=search_whole_words_only) == False
+    assert boolean_search(one_hundred_animals_string, AND_list=["non_existent_word", "rabbit"], search_whole_words_only=search_whole_words_only) == False
 
-def test_or_only_cases():
-    assert boolean_search("dog mouse", [], ["mouse", "cat"]) == True
-    assert boolean_search("dog mouse", [], ["elephant", "giraffe"]) == False
-    assert boolean_search("lizard snake", [], ["snake"], []) == True
-    assert boolean_search("lizard snake", [], ["rabbit"], []) == False
 
-def test_not_only_cases():
-    assert boolean_search("dog mouse", [], [], ["mouse"]) == False
-    assert boolean_search("dog mouse", [], [], ["cat"]) == True
-    
-def test_and_or_combinations():
+def test_or_only_cases(search_whole_words_only = False):
+    assert boolean_search("dog mouse", OR_list=["mouse", "cat"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search(one_hundred_animals_string, OR_list=["cat", "non_existent_word"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search("dog mouse", OR_list=["elephant", "giraffe"], search_whole_words_only=search_whole_words_only) == False
+    assert boolean_search(one_hundred_animals_string, OR_list=["non_existent_word", "non_existent_word_1"], search_whole_words_only=search_whole_words_only) == False
+    assert boolean_search("lizard snake", OR_list=["snake"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search("lizard snake", OR_list=["rabbit"]) == False
+
+def test_not_only_cases(search_whole_words_only = False):
+    assert boolean_search("dog mouse", NOT_list=["mouse"], search_whole_words_only=search_whole_words_only) == False
+    assert boolean_search(one_hundred_animals_string, NOT_list=["cat"], search_whole_words_only=search_whole_words_only) == False
+    assert boolean_search("dog mouse", NOT_list=["cat"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search(one_hundred_animals_string, NOT_list=["non_existent_word"], search_whole_words_only=search_whole_words_only) == True
+
+def test_and_or_combinations(search_whole_words_only = False):
     # AND and OR both match
     assert boolean_search("dog rabbit", ["dog"], ["rabbit"]) == True
+    assert boolean_search(one_hundred_animals_string, ["dog"], ["rabbit"]) == True
     # AND matches but OR fails
-    assert boolean_search("dog cat", ["dog"], ["mouse", "rabbit"]) == False
+    assert boolean_search("dog cat", ["dog", "cat"], ["mouse", "rabbit"]) == False
+    assert boolean_search(one_hundred_animals_string, ["dog", "cat"], ["non_existent_word", "non_existent_word_1"]) == False
     # OR matches but AND fails
-    assert boolean_search("dog mouse", ["rabbit"], ["mouse"], []) == False
-    # OR matches but AND fails, explicit
-    assert boolean_search("mouse elephant", ["dog"], ["mouse"], []) == False
-    # AND matches, no OR list
-    assert boolean_search("dog rabbit", ["dog"], [], []) == True
-    # OR matches, no AND list
-    assert boolean_search("dog rabbit", [], ["rabbit"], []) == True
+    assert boolean_search("dog mouse", ["rabbit", "dog"], ["mouse"], []) == False
+    assert boolean_search(one_hundred_animals_string, ["non_existent_word", "dog"], ["cat"], []) == False
 
-
-def test_and_or_not_combinations():
-    # All match, NOT passes
+#TODO add one_hundred_animals_string test starting from below
+def test_and_or_not_combinations(search_whole_words_only = False):
+    # All and OR match, NOT passes
     assert boolean_search("dog rabbit", ["dog"], ["rabbit"], ["cat"]) == True
-    # AND and OR match, but NOT excludes
+    # AND and OR match, NOT fails
     assert boolean_search("dog rabbit cat", ["dog"], ["rabbit"], ["cat"]) == False
     # AND matches, OR fails, NOT passes
     assert boolean_search("dog rabbit", ["dog"], ["mouse"], ["cat"]) == False
@@ -51,13 +57,15 @@ def test_and_or_not_combinations():
     assert boolean_search("dog rabbit", ["elephant"], ["mouse"], ["cat"]) == False
 
 
-def test_no_filters_returns_true():
-    assert boolean_search("any text here", [], [], []) == True
+def test_no_filters_returns_true(search_whole_words_only = False):
+    assert boolean_search("any text here", search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search(one_hundred_animals_string, search_whole_words_only=search_whole_words_only) == True
 
+def test_examples_from_prompt(search_whole_words_only = False):
+    assert boolean_search("cat dog rabbit", ["cat", "rabbit"], search_whole_words_only=search_whole_words_only) == True
+    assert boolean_search("dog mouse", ["cat", "rabbit"], search_whole_words_only=search_whole_words_only) == False
 
-def test_examples_from_prompt():
-    assert boolean_search("cat dog rabbit", ["cat", "rabbit"]) == True
-    assert boolean_search("dog mouse", ["cat", "rabbit"]) == False
+start_time = time.time()
 
 test_basic_and_cases()
 test_or_only_cases()
@@ -66,3 +74,21 @@ test_and_or_combinations()
 test_and_or_not_combinations()
 test_no_filters_returns_true()
 test_examples_from_prompt()
+
+end_time = time.time()
+
+print(f"all substring tests passed in {end_time - start_time:.6f} seconds")
+
+start_time = time.time()
+
+test_basic_and_cases(search_whole_words_only = True)
+test_or_only_cases(search_whole_words_only = True)
+test_not_only_cases(search_whole_words_only = True)
+test_and_or_combinations(search_whole_words_only = True)
+test_and_or_not_combinations(search_whole_words_only = True)
+test_no_filters_returns_true(search_whole_words_only = True)
+test_examples_from_prompt(search_whole_words_only = True)
+
+end_time = time.time()
+
+print(f"all whole word tests passed in {end_time - start_time:.6f} seconds")
