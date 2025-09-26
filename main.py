54d27da7ee75415658cb199452620abe344b37c4
Samuel <48211508+SamuelYiuSY@users.noreply.github.com>
2025-09-26 14:39:30 +0100

    allow search_wholes_only for potential speedup

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def boolean_search(string_to_search, AND_list = None, OR_list = None, NOT_list = None):
+def boolean_search(search_string, AND_list = None, OR_list = None, NOT_list = None, search_whole_words_only = False):
+    if search_whole_words_only == True:
+        # convert string_to_search from string to set to speed up
+        search_string = set(search_string.split())
+    
     # check OR and NOT list first as they may return before going through the whole string
     if OR_list:
         have_1_OR_word_flag = False
         for eachWord in OR_list:
-            if eachWord in string_to_search:
+            if eachWord in search_string:
                 have_1_OR_word_flag = True
                 break
         if not have_1_OR_word_flag:
@@ -12,8 +16,14 @@ def boolean_search(string_to_search, AND_list = None, OR_list = None, NOT_list =
     
     if NOT_list:
         for eachWord in NOT_list:
-            if eachWord in string_to_search:
+            if eachWord in search_string:
                 # print("NOT statement not satisified")
+                return False
+
+    if AND_list:
+        for eachWord in AND_list:
+            if eachWord not in search_string:
+                # print("AND statement not satisified")
                 return False
     
     return True
